3cd5a8cf4d626a845fab0079f907603ff9de199b
Anthony Langford <anthony.langford@gmail.com>
2018-06-14 00:48:31 -0400

    styled components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import styled from 'react-emotion'
 
 // Import helpers
 import store from '../store'
 import { commonActions } from '../actions'
+
+const Card = styled('div')`
+  display: flex;
+  overflow: scroll;
+  padding: 4px;
+  border: 1px solid rgba(0,0,0,0.2);
+  border-radius: 7px;
+  box-shadow: 0px 1px 0 #d10000,
+    2px 2px 0 #FFC107,
+    3px 4px 0 #019DC9;
+  transition: 0.3s;
+  :hover {
+    box-shadow: 0 8px 20px 0 rgba(0,0,0,0.3);
+    border: 1px solid rgba(0,50,0,0.6);
+  }
+`
+
+const Column = styled('div')`
+  display: flex;
+  flex-direction: column;
+  margin: 4px;
+`
+
+const Text = styled('span')`
+  font-size: 12px;
+`
+
+const Title = styled('h1')`
+  font-size: 1.1em;
+  font-weight: 500;
+`
 
 class Home extends React.Component {
   constructor(props) {
@@ -30,7 +62,7 @@ class Home extends React.Component {
   }
 
   render() {
-    const wine = this.state.wine;
+    const wine = this.state.wine
 
     return (
       !wine ? (
@@ -44,21 +76,50 @@ class Home extends React.Component {
           height: 100vh;
           padding: 1rem;
         `}>
-          <div css={`
-            display: flex;
-            height: 200px;
-            width: 320px;
-            padding: 0.5rem;
-            border: 1px solid black;
-            border-radius: 7px;
-          `}>
-            <img src={wine.image_thumb_url} height="140px" />
-            <div css={`
-            display: flex;
-          `}>
-              <span>Name: {wine.name}</span>
-            </div>
-          </div>
+          <Card>
+            <Column>
+              <img src={wine.image_thumb_url} height="200px" />
+              <Text>
+                Price: ${wine.price_in_cents/100}
+              </Text>
+
+              <Text>
+                Volume: {wine.volume_in_milliliters}mL
+              </Text>
+
+              <Text>
+                ABV: {wine.alcohol_content/100}%
+              </Text>
+
+              <Text>
+                Origin: {wine.origin}
+              </Text>
+
+              <Text>
+                Category: {wine.secondary_category}
+              </Text>
+
+              <Text>
+                Type: {wine.varietal}
+              </Text>
+              <Text>
+                Style: {wine.style}
+              </Text>
+              <Text>
+                Producer: {wine.producer_name}
+              </Text>
+            </Column>
+
+            <Column>
+              <Title>{wine.name}</Title>
+
+              <Text css={`
+                overflow: scroll;
+              `}>
+                Description: {wine.tasting_note}
+              </Text>
+            </Column>
+          </Card>
         </div>
       )
     )
