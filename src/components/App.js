277391b26725122dbadfee93b81ac0d02225c11b
Anthony Langford <anthony.langford@gmail.com>
2018-06-14 00:47:49 -0400

    ✨

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,6 @@ import '../styles/normalize.css'
 import { commonActions } from '../actions'
 
 // Import containers
-// import TestContainer from '../components/test/TestContainer'
 import HomeContainer from './Home'
 
 store.subscribe(() => {})
@@ -16,13 +15,11 @@ store.subscribe(() => {})
 store.dispatch(commonActions.fetchWines())
 
 const Home = () => <HomeContainer />
-// const Test = () => <TestContainer />
 
 const App = () =>
   <Provider store={store}>
     <Router>
       <Home path="/" />
-      {/* <Test path="/" /> */}
     </Router>
   </Provider>
 
