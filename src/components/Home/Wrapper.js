ccf622000c2eb14ac344cea9e508d7a14bb9b24b
Anthony Langford <anthony.langford@gmail.com>
2018-06-18 17:28:17 -0400

    create wrapper component

---
+++ src/components/Home/Wrapper.js
@@ -0,0 +1,11 @@
+import styled from 'react-emotion'
+
+const Wrapper = styled('div')`
+  display: flex;
+  flex-flow: row wrap;
+  height: 100vh;
+  padding: 1rem;
+  overflow: scroll;
+`
+
+export default Wrapper
