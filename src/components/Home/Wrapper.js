a85dc5f8e257e96e29822ace24b94c7df8d31a57
Anthony Langford <anthony.langford@gmail.com>
2018-06-20 14:06:33 -0400

    add ugly linear gradient background

--- src/components/Home/Wrapper.js
+++ src/components/Home/Wrapper.js
@@ -6,6 +6,7 @@ const Wrapper = styled('div')`
   height: 100vh;
   padding: 1rem;
   overflow: scroll;
+  background: linear-gradient(170deg, rgba(33,150,243,.95) 10%, rgba(77,208,225,.95) 80%);
 `
 
 export default Wrapper
