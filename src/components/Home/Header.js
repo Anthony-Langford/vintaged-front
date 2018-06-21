ef7ef4829137a25885127dc7e912666289b4531e
Anthony Langford <anthony.langford@gmail.com>
2018-06-21 18:13:25 -0400

    create header component

---
+++ src/components/Home/Header.js
@@ -0,0 +1,54 @@
+//emotion
+import React from 'react'
+
+// Import components
+import Logo from './Logo'
+
+function Header() {
+  return(
+    <div
+      label="Header"
+      css={`
+        background: #1D2029;
+      `}
+    >
+      <div
+        css={`
+          display: flex;
+          flex: 1;
+          overflow: auto;
+          justify-content: space-between;
+          padding: 16px;
+        `}
+      >
+        <div
+          css={`
+            display: inline;
+            flex: 1;
+            align-items: center;
+            opacity: 1;
+            transition: opacity 200ms ease;
+            width: min-content;
+          `}
+        >
+          <nav>
+            <Logo />
+          </nav>
+        </div>
+        <h1
+          css={`
+            margin: 0 0 0 8px;
+            width: 100%;
+            font-family: 'Oswald';
+            font-size: 32px;
+            color: white;
+          `}
+        >
+          Vintaged
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header
