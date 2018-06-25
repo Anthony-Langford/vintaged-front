e2d4be5cf66237aa2bc5fa472dfbe5c85e9c9f67
Anthony Langford <anthony.langford@gmail.com>
2018-06-24 23:07:39 -0400

    change overflow from scroll to auto in wrapper component 🍬

--- src/components/Home/Wrapper.js
+++ src/components/Home/Wrapper.js
@@ -7,7 +7,7 @@ const Wrapper = styled('div')`
   flex-flow: row wrap;
   width: 100%;
   min-height: 100%;
-  overflow: scroll;
+  overflow: auto;
   color: black;
   // background: linear-gradient(170deg, rgba(230,230,250,.05) 20%, rgba(114,47,55,.05) 10%);
 `
