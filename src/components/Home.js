e15688a9bc73a15484a21dd6ae4df614c322ec00
Anthony Langford <anthony.langford@gmail.com>
2018-06-26 17:30:11 -0400

    move navigsation logo to module

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux'
 import Header from './Home/Header'
 import Wrapper from './Home/Wrapper'
 import ProductCardsList from './Home/ProductCardsList'
+import NavigationLogo from './Home/NavigationLogo'
 
 // Import helper functions
 import getCurrentLocation from '../helpers/getCurrentLocation'
@@ -45,38 +46,7 @@ class Home extends React.Component {
             <ProductCardsList wines={wines} />
           )}
         </Wrapper>
-        <button
-          css={`
-            position: fixed;
-            bottom: 0px;
-            right: 0px;
-            background-color: #AD1457;
-            border-radius: 50%;
-            padding: 16px;
-            margin: 32px;
-            animation: animation-1m49nxd 0.25s ease-in;
-            transition: background-color 150ms ease-in-out;
-            border-width: 0px;
-            border-style: initial;
-            border-color: initial;
-            border-image: initial;
-          `}
-        >
-          <svg
-            fill="currentColor"
-            preserveAspectRatio="xMidYMid meet"
-            height="32"
-            width="32"
-            viewBox="0 0 40 40"
-            style={{"verticalAlign": "middle", "color": "white"}}
-          >
-            <title>Navigation Logo</title>
-            <g>
-              <path d="m5 10h30v3.4h-30v-3.4z m0 11.6v-3.2h30v3.2h-30z m0 8.4v-3.4h30v3.4h-30z">
-              </path>
-            </g>
-          </svg>
-        </button>
+        <NavigationLogo />
       </React.Fragment>
     )
   }
