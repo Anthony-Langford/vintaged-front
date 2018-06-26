553bcacbeccdbede7ace4ec8d0ff89238b940b93
Anthony Langford <anthony.langford@gmail.com>
2018-06-26 14:35:03 -0400

    change overflow to hidden for nav

--- src/components/Home/Wrapper.js
+++ src/components/Home/Wrapper.js
@@ -7,9 +7,8 @@ const Wrapper = styled('div')`
   flex-flow: row wrap;
   width: 100%;
   min-height: 100%;
-  overflow: auto;
+  overflow: hidden;
   color: black;
-  // background: linear-gradient(170deg, rgba(230,230,250,.05) 20%, rgba(114,47,55,.05) 10%);
 `
 
 export default Wrapper
