63b32406399b8dc179f1d6b825d04b2171b4e428
Anthony Langford <anthony.langford@gmail.com>
2018-06-25 21:25:54 -0400

    move geolocation from app root to module

--- src/components/App.js
+++ src/components/App.js
@@ -12,31 +12,7 @@ import HomeContainer from './Home'
 
 store.subscribe(() => {})
 
-if ("geolocation" in navigator) {
-  /* geolocation is available */
-  const successCallback = position => {
-    const lat = position.coords.latitude
-    const lon = position.coords.longitude
-
-    store.dispatch(commonActions.receiveLocation(lat, lon))
-    store.dispatch(commonActions.fetchWines(lat, lon))
-    
-    console.log(`I know where you are 🤫🤡🤐👽👀👁👁🔭 lat ${lat}°, lon ${lon}°`)
-  }
-
-  const errorCallback = error => {
-    console.log('ERROR(' + error.code + '): ' + error.message);
-    store.dispatch(commonActions.fetchWines())
-  }
-
-  const options = { timeout: 27000 }
-
-  navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options)
-} else {
-  /* geolocation is not available */
-  console.log('Geolocation is not available')
-  store.dispatch(commonActions.fetchWines())
-}
+store.dispatch(commonActions.fetchWines())
 
 const Home = () => <HomeContainer />
 
