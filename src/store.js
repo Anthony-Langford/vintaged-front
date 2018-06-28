41ce8f5bc93f6647d683a1229f5b8c56b9eaafaa
Anthony Langford <anthony.langford@gmail.com>
2018-06-28 11:36:26 -0700

    🚿

--- src/store.js
+++ src/store.js
@@ -1,19 +1,19 @@
-import {createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 // https://medium.com/@gethylgeorge/understanding-how-redux-thunk-works-72de3bdebc50
 // Infographic on redux thunk https://cdn-images-1.medium.com/max/1600/1*eByw4NguNH8nK0MA_Q5Fow.png
 import thunkMiddleware from 'redux-thunk';
-// import {routerMiddleware} from 'react-router-redux';
-// import {createBrowserHistory} from 'history';
+// import { routerMiddleware } from 'react-router-redux';
+// import { createBrowserHistory } from 'history';
 
 // Import the root reducer
 import rootReducer from './reducers/index';
 
 // Create an object for the default state
-const defaultState = {
+const initialState = {
   common: {
     wines: []
   }
-};
+}
 
 // TODO: after react router is implemented
 // Create a browser history
@@ -23,28 +23,20 @@ const defaultState = {
 // const middleware = routerMiddleware(history);
 
 // Customize middleware depending on environment
-const environmentMiddleware = (process.env.NODE_ENV === 'development') ? (
+const environmentMiddleware = 
   applyMiddleware(
     // ...middleware,
     thunkMiddleware
   )
-) : (
-  applyMiddleware(
-    // ...middleware
-  )
-);
 
 // TODO: after react router is implemented
 // Add the reducer to your store on the `router` key
 // Also apply our middleware for navigating
-/* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  defaultState,
+  initialState,
   composeEnhancers(environmentMiddleware)
-);
-
-/* eslint-enable */
+)
 
 export default store;
