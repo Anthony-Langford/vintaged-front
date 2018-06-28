a0e293b1306aa292d547e0033be1ad287a3b57ea
Anthony Langford <anthony.langford@gmail.com>
2018-06-28 11:37:28 -0700

    🔦

--- src/components/Home/ProductCardsList.js
+++ src/components/Home/ProductCardsList.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types'
 // Import components
 import ProductCard from './ProductCard'
 
-function ProductCardsList(props) {
-  const wines = props.wines
+export default function ProductCardsList({ wines }) {
 
+  // Filter by store-specific inventory data if possible
   const filteredWines = wines[0].store_LAPI ?
     wines.filter(wine => wine.store_LAPI[0].quantity > 0).slice(0, 5) :
     wines.slice(0, 5)
@@ -28,5 +28,3 @@ function ProductCardsList(props) {
 ProductCardsList.propTypes = {
   wines: PropTypes.array.isRequired,
 }
-
-export default ProductCardsList
