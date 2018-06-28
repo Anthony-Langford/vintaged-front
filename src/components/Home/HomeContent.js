73088f28d4e4d56fc9fe026bd8e4c446508d805f
Anthony Langford <anthony.langford@gmail.com>
2018-06-28 01:17:03 -0700

    create nav bar and overlay

--- src/components/Home/HomeContent.js
+++ src/components/Home/HomeContent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from "@reach/router";
 
 // Import components
 import ProductCardsList from './ProductCardsList'
@@ -8,6 +9,8 @@ import NavigationLogo from './NavigationLogo'
 function HomeContent(props) {
   const wines = props.wines
   const winesFetched = props.winesFetched
+  const openNav = props.openNav
+  const toggleNav = props.toggleNav
   
   return(
     <div css={`
@@ -36,8 +39,65 @@ function HomeContent(props) {
               <ProductCardsList wines={wines} />
             )}
           </div>
+          <div onClick={toggleNav} css={`
+            z-index: 2;
+            position: absolute;
+            top: 0px;
+            bottom: 0px;
+            transition: -webkit-transform 0.3s cubic-bezier(0.785, 0.135, 0.15, 0.86);
+            will-change: transform;
+            overflow-y: auto;
+            right: 0px;
+            transform: translateX(${openNav ? 0 : 100}%);
+            background-color: #f5f5f5;
+            padding: 32px;
+            width: 250px;
+          `}>
+            <h3>Navigation</h3>
+            <nav>
+              <Link
+                to="/#"
+                css={`
+                  display: block;
+                  -webkit-text-decoration: none;
+                  text-decoration: none;
+                  margin: 16px;
+                  font-size: 16px;
+                  color: inherit;
+                `}
+              >
+                Home
+              </Link>
+              <Link
+                to="/test"
+                css={`
+                  display: block;
+                  -webkit-text-decoration: none;
+                  text-decoration: none;
+                  margin: 16px;
+                  font-size: 16px;
+                  color: inherit;
+                `}
+              >
+                Test
+              </Link>
+            </nav>
+          </div>
         </div>
-        <NavigationLogo />
+        <div onClick={toggleNav} css={`
+            z-index: 1;
+            position: fixed;
+            top: 0px;
+            left: 0px;
+            right: 0px;
+            bottom: 0px;
+            opacity: ${openNav ? 1 : 0};
+            visibility: ${openNav ? 'visible' : 'hidden'};
+            transition: opacity 0.3s ease-out, visibility 0.3s ease-out;
+            background-color: rgba(0, 0, 0, 0.3);
+          `}>
+        </div>
+        <NavigationLogo onClick={toggleNav} />
       </div>
     </div>
   )
@@ -46,7 +106,9 @@ function HomeContent(props) {
 // Static type checking for props
 HomeContent.propTypes = {
   wines: PropTypes.array.isRequired,
-  winesFetched: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]).isRequired
+  winesFetched: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]).isRequired,
+  openNav: PropTypes.bool.isRequired,
+  toggleNav: PropTypes.func.isRequired
 }
 
 export default HomeContent
