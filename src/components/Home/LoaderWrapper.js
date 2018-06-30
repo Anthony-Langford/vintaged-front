b9fab987093b832baf70dd5b6fe36e691f4a7400
Anthony Langford <anthony.langford@gmail.com>
2018-06-30 07:47:31 -0700

    create loader wrapper 🍬

---
+++ src/components/Home/LoaderWrapper.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function LoaderWrapper({
+  winesFetched,
+  children
+}) {
+  return (
+    winesFetched ? (
+      children
+    ) : <div>Loading...</div>
+  )
+}
+
+LoaderWrapper.propTypes = {
+  winesFetched: PropTypes.bool,
+  children: PropTypes.node
+}
