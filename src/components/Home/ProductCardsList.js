63924f6f1088be2786c2a4e8140be08c19f325ce
Anthony Langford <anthony.langford@gmail.com>
2018-07-06 17:39:33 -0400

    add filtering

--- src/components/Home/ProductCardsList.js
+++ src/components/Home/ProductCardsList.js
@@ -5,7 +5,8 @@ import PropTypes from 'prop-types'
 import ProductCard from './ProductCard'
 
 export default function ProductCardsList({
-  wines
+  wines,
+  filters
 }) {
 
   // Filter by store-specific inventory data if possible
@@ -13,8 +14,23 @@ export default function ProductCardsList({
   //   wines.filter(wine => wine.store_LAPI[0].quantity > 0).slice(0, 5) :
   //   wines.slice(0, 5)
 
-  const filteredWines = wines.slice(0, 5)
+  // const filteredWines = Object.keys(filters) > 0 ?
+  //   wines.filter(wine => !(filters.indexOf(wine.secondary_category) + 1)).slice(0, 5) :
+  //   wines.slice(0, 5)
 
+  let filteredWines = wines
+
+  // Iterate through filters and filter if any are selected
+  for(const key in filters) {
+    if (filters[key].length > 0) {
+      filteredWines = filteredWines.filter(wine => {
+        return filters[key].indexOf(wine[key]) > -1
+      })
+    }
+  }
+
+  const topFiveFilteredWines = filteredWines.slice(0, 5)
+  
   return(
     <div
       label="ProductCardsList"
@@ -23,7 +39,7 @@ export default function ProductCardsList({
         flex-flow: row wrap;
       `}
     >
-      {filteredWines.map(wine => <ProductCard wine={wine} key={wine.id} />)}
+      {topFiveFilteredWines.map(wine => <ProductCard wine={wine} key={wine.id} />)}
     </div>
   )
 }
@@ -31,4 +47,5 @@ export default function ProductCardsList({
 // Static type checking for props
 ProductCardsList.propTypes = {
   wines: PropTypes.array.isRequired,
+  filters: PropTypes.object
 }
