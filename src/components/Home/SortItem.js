298c0596b622729fb3349eff46aa7c8d7a9e5cb9
Anthony Langford <anthony.langford@gmail.com>
2018-07-07 12:06:09 -0400

    create sort item component

--- src/components/Home/SortItem.js
+++ src/components/Home/SortItem.js
@@ -1,23 +1,51 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'react-emotion'
 
 export default function SortItem({
   title,
-  selected,
   value,
+  selected,
   onClick
 }) {
+
+  const ListItem = styled('li')`
+    list-style: none;
+    margin: 0 8px;
+    width: calc(100% + 20px);
+  `
+
+  const Button = styled('button')`
+    padding: 0.25rem;
+    font-weight: ${selected === value ? 'bold' : 'normal'};
+    border: none;
+    background-color: White;  
+    border-radius: 5px;
+    &:focus,
+    &:hover {
+      font-weight: bold;
+      cursor: pointer;
+    }
+  `
+
   return(
-    selected ?
-      <button onClick={onClick} value={value} css={`margin: 0 8px; padding: 0.25rem; font-weight: bold; &:hover {cursor: pointer}`}>{title}</button> :
-      <button onClick={onClick} value={value} css={`margin: 0 8px; padding: 0.25rem; &:hover {cursor: pointer}`}>{title}</button>
+    <ListItem>
+      <Button
+        href="#"
+        aria-controls="content-0"
+        onClick={onClick}
+        value={value}
+      >
+        {title}
+      </Button>
+    </ListItem>
   )
 }
 
 // Static type checking for props
 SortItem.propTypes = {
   title: PropTypes.string.isRequired,
-  selected: PropTypes.bool.isRequired,
+  selected: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired
 }
