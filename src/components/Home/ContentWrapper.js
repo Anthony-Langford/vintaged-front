cd10666a5dca4ca94aa5e2a1474663e24e612237
Anthony Langford <anthony.langford@gmail.com>
2018-07-08 17:42:10 -0400

    better name

---
+++ src/components/Home/ContentWrapper.js
@@ -0,0 +1,11 @@
+import styled from 'react-emotion'
+
+export default styled('div')`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  margin-top: 68px;
+  color: #1e2029;
+  background: #FFFEFF;
+  border-radius: 2px;
+`
