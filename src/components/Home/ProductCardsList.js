f89e97e06f2a2c341d9d2f4cf10eedb6be1d0384
Anthony Langford <anthony.langford@gmail.com>
2018-07-09 19:19:45 -0400

    add direction support to sorting

--- src/components/Home/ProductCardsList.js
+++ src/components/Home/ProductCardsList.js
@@ -6,22 +6,16 @@ import ProductCard from './ProductCard'
 
 export default function ProductCardsList({
   wines,
-  sortKey,
+  sortBy,
+  sortDirection,
   filters
 }) {
-
+  let filteredWines = wines
   // Filter by store-specific inventory data if possible
   // const filteredWines = wines[0].store_LAPI ?
   //   wines.filter(wine => wine.store_LAPI[0].quantity > 0).slice(0, 5) :
   //   wines.slice(0, 5)
 
-  // const filteredWines = Object.keys(filters) > 0 ?
-  //   wines.filter(wine => !(filters.indexOf(wine.secondary_category) + 1)).slice(0, 5) :
-  //   wines.slice(0, 5)
-
-  let filteredWines = wines
-
-  // TODO: Allow user to sort in both directions
   // Iterate through filters and filter if any are selected
   for(const key in filters) {
     if (filters[key].length > 0) {
@@ -31,28 +25,41 @@ export default function ProductCardsList({
     }
   }
 
+  filteredWines = filteredWines.slice(0, 5)
+
+  // Sort wines by selected sorting method
   let mapped = filteredWines.map((wine, i) => {
     return {
       index: i,
-      value: wine[sortKey]
+      value: wine[sortBy]
     }
   })
 
-  mapped.sort((a, b) => {
-    if (b.value > a.value) {
-      return 1
-    }
-    if (a.value > b.value) {
-      return -1
-    }
-    return 0
-  })
+  if (sortDirection === 'ascending') {
+    mapped.sort((a, b) => {
+      if (a.value > b.value) {
+        return 1
+      }
+      if (b.value > a.value) {
+        return -1
+      }
+      return 0
+    })
+  } else {
+    mapped.sort((a, b) => {
+      if (a.value > b.value) {
+        return -1
+      }
+      if (b.value > a.value) {
+        return 1
+      }
+      return 0
+    })
+  }
 
-  const sortedFilteredWines = mapped.map((el) => {
-    return filteredWines[el.index];
-  });
-
-  const topFiveWines = sortedFilteredWines.slice(0, 5)
+  const topFiveWines = mapped.map(el => (
+    filteredWines[el.index]
+  ))
   
   return(
     <div
@@ -70,6 +77,7 @@ export default function ProductCardsList({
 // Static type checking for props
 ProductCardsList.propTypes = {
   wines: PropTypes.array.isRequired,
-  sortKey: PropTypes.string.isRequired,
+  sortBy: PropTypes.string.isRequired,
+  sortDirection: PropTypes.string.isRequired,
   filters: PropTypes.object
 }
