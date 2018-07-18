26c5a62fb64043f9a39554291990c685b637fbb5
Anthony Langford <anthony.langford@gmail.com>
2018-07-18 15:44:53 -0400

    move nav button to header

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,53 +1,68 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 // Import components
 import Logo from './Logo'
+import NavButton from './NavButton'
 
-export default () => (
-  <div
-    label="Header"
-    css={`
-      background: #1D2029;
-      z-index: 2;
-      width: 100%;
-      top: 0;
-      left: 0;
-    `}
-  >
+export default function Header ({
+  toggleNav
+}) {
+  return(
     <div
+      label="Header"
       css={`
-        display: flex;
-        flex: 1;
-        overflow: auto;
-        justify-content: space-between;
-        padding: 16px;
+        background: #1D2029;
+        z-index: 3;
+        width: 100%;
+        top: 0;
+        left: 0;
       `}
     >
       <div
         css={`
-          display: inline;
+          display: flex;
           flex: 1;
-          align-items: center;
-          opacity: 1;
-          transition: opacity 200ms ease;
-          width: min-content;
+          overflow: auto;
+          justify-content: space-between;
+          padding: 16px;
         `}
       >
-        <nav>
-          <Logo />
-        </nav>
+        <div
+          css={`
+            display: inline;
+            flex: 1;
+            align-items: center;
+            opacity: 1;
+            transition: opacity 200ms ease;
+            width: min-content;
+          `}
+        >
+          <nav css={`
+            display: flex;
+          `}
+          >
+            <NavButton onClick={toggleNav} />
+          </nav>
+        </div>
+        <h1
+          css={`
+            margin: 0 0 0 8px;
+            width: 100%;
+            font-family: Cursive;
+            font-size: 32px;
+            color: #f5f5f5;
+          `}
+        >
+          Vintaged
+        </h1>
+        <Logo />
       </div>
-      <h1
-        css={`
-          margin: 0 0 0 8px;
-          width: 100%;
-          font-family: Cursive;
-          font-size: 32px;
-          color: #f5f5f5;
-        `}
-      >
-        Vintaged
-      </h1>
     </div>
-  </div>
-)
+  )
+}
+
+// Static type checking for props
+Header.propTypes = {
+  toggleNav: PropTypes.func.isRequired
+}
