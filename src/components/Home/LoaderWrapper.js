76291ede83a231357021aab67bf425774b74282b
Anthony Langford <anthony.langford@gmail.com>
2018-07-21 12:06:39 -0400

    //

--- src/components/Home/LoaderWrapper.js
+++ src/components/Home/LoaderWrapper.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+// Wine glass filling up loader
+// https://codepen.io/wolfvanveen/pen/zopKqN
+
 export default function LoaderWrapper({
   winesFetched,
   children
