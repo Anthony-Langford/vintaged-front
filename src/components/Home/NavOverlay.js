0f2b4b76c34d53d5b577eb5a6c768448af9bddd1
Anthony Langford <anthony.langford@gmail.com>
2018-07-22 20:12:29 -0400

    change nav state key name

--- src/components/Home/NavOverlay.js
+++ src/components/Home/NavOverlay.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 export default function NavOverlay({
   toggleNav,
-  navState
+  navOpen
 }) {
   return(
     <div
@@ -16,8 +16,8 @@ export default function NavOverlay({
         left: 0px;
         right: 0px;
         bottom: 0px;
-        opacity: ${navState ? 1 : 0};
-        visibility: ${navState ? 'visible' : 'hidden'};
+        opacity: ${navOpen ? 1 : 0};
+        visibility: ${navOpen ? 'visible' : 'hidden'};
         transition: opacity 0.3s ease-out, visibility 0.3s ease-out;
         background-color: rgba(0, 0, 0, 0.3);
       `}
@@ -27,5 +27,5 @@ export default function NavOverlay({
 
 NavOverlay.propTypes = {
   toggleNav: PropTypes.func.isRequired,
-  navState: PropTypes.bool.isRequired
+  navOpen: PropTypes.bool.isRequired
 }
