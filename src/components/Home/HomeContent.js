677a66d2f55d0bdb59d8a6a8380ce636c311cd2b
Anthony Langford <anthony.langford@gmail.com>
2018-07-24 13:50:51 -0400

    move front page to module

---
+++ src/components/Home/HomeContent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function HomeContent({
+  
+}) {
+  return(
+    <div
+      css={`
+        display: flex;
+        flex-direction: column;
+        margin: 0 1rem;
+      `}
+    >
+      <h1 css={`font-weight: 300;`}>Explore Vintaged and Find Your Wine</h1>
+    </div>
+  )
+}
