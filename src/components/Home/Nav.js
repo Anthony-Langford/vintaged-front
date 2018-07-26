fd6b55b53e260589a2bc1ed85cd513e19518f7c9
Anthony Langford <anthony.langford@gmail.com>
2018-07-26 12:30:31 -0400

    add thyrsus to navbar

--- src/components/Home/Nav.js
+++ src/components/Home/Nav.js
@@ -13,12 +13,14 @@ export default function Nav({
       label="NavBar"
       onClick={toggleNav}
       css={`
+        display: flex;
+        justify-content: space-between;
         z-index: 1001;
         position: absolute;
         top: 0px;
         bottom: 0px;
         left: -250px;
-        padding: 32px;
+        padding: 32px 0 32px 32px;
         width: 250px;
         transition: transform 0.3s cubic-bezier(0.785, 0.135, 0.15, 0.86);
         will-change: transform;
@@ -27,10 +29,39 @@ export default function Nav({
         background-color: #f5f5f5;
       `}
     >
-      <h3>Navigation</h3>
-      <nav>
-        <NavLinks navOpen={navOpen} />
-      </nav>
+      <div css={`max-width: 350px;`}>
+        <h3>Navigation</h3>
+        <nav>
+          <NavLinks navOpen={navOpen} />
+        </nav>
+      </div>
+      <div
+        css={`
+          position: relative;
+          overflow: hidden;
+          display: inline-block;
+          padding: 0;
+          width: 100%;
+          height: auto;
+        `}
+      >
+        <img
+          alt="vintaged"
+          src="/thyrsus.jpg"
+          css={`
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            transition: opacity 0.5s;
+            width: 120%;
+            margin-left: -18px;
+            object-fit: cover;
+            object-position: 50% 50%;
+            opacity: 1;
+            mix-blend-mode: multiply;
+          `}
+        />
+      </div>
     </div>
   )
 }
