7629ebb41b6afec9bedb085cf1f38931db9d5d4f
Anthony Langford <anthony.langford@gmail.com>
2018-08-02 15:25:03 -0400

    create sort reducer

--- src/reducers/sort.js
+++ src/reducers/sort.js
@@ -1,19 +1,37 @@
 export default function sort(
   state = {
-    sortKey: {
-      sortBy: '',
-      sortDirection: '',
-    },
+    sortBy: 'heat',
+    sortDirection: 'descending',
+    sortList: [
+      {
+        id: 0,
+        title: 'Heat',
+        value: 'heat',
+        sortDirection: 'descending'
+      },
+      {
+        id: 1,
+        title: 'Price',
+        value: 'price_in_cents',
+        sortDirection: 'ascending'
+      }
+    ],
   },
-  action) {
+  action
+) {
   switch (action.type) {
   case 'SET_SORT' : {
     return {
       ...state,
-      sortKey: {
-        sortBy: action.sortBY,
-        sortDirection: action.sortDirection,
-      },
+      sortBy: action.sortBy,
+      sortDirection: action.sortDirection,
+      sortList: action.sortList
+    };
+  }
+  case 'SET_SORT_LIST' : {
+    return {
+      ...state,
+      sortList: action.sortList
     };
   }
   default : {
