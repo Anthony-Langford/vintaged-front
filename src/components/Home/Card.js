2e49505de48020a69b1a52887f78383c67a7eb24
Anthony Langford <anthony.langford@gmail.com>
2018-08-07 14:46:43 -0400

    remove hover animation from card

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -14,9 +14,4 @@ export default styled('div')`
   box-shadow: 0px 4px 1px -2px rgba(0,0,0,0.2),
     0px 2px 2px 0px rgba(0,0,0,0.14),
     0px 1px 5px 0px rgba(0,0,0,0.12);
-  transition: 0.3s;
-  :hover {
-    box-shadow: 0 8px 20px 0 rgba(0,0,0,0.3);
-    border: 1px solid rgba(29,32,41,0.7);
-  }
 `
