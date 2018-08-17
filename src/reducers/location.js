63fd459621cb91a8dc6dba69a9e39db280782786
Anthony Langford <anthony.langford@gmail.com>
2018-08-17 07:14:22 -0400

    cleanup ✨

--- src/reducers/location.js
+++ src/reducers/location.js
@@ -1,19 +1,16 @@
-export default function location(
-  state = {},
-  action) {
+export default function location(state = {}, action) {
   switch (action.type) {
-  case 'RECEIVE_LOCATION' : {
+  case 'RECEIVE_LOCATION':
     return {
       ...state,
       lat: action.lat,
       lon: action.lon
     }
-  }
-  case 'CLEAR_STORE' : {
+
+  case 'CLEAR_STORE':
     return {};
-  }
-  default: {
+    
+  default:
     return state;
-  }
   }
 }
