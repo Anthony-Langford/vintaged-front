6e0cf5d6c28bafbd5baa5dd65bd1816065c47623
Anthony Langford <anthony.langford@gmail.com>
2018-08-17 07:15:02 -0400

    cleanup ✨

--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -2,16 +2,16 @@ export default function ui(
   state = {
     navOpen: false,
   },
-  action) {
+  action
+) {
   switch (action.type) {
-  case 'TOGGLE_NAV' : {
+  case 'TOGGLE_NAV':
     return {
       ...state,
       navOpen: action.toggleNav
-    };
-  }
-  default : {
-    return state;
-  }
+    }
+
+  default:
+    return state
   }
 }
