812fae83b50099f9999222676f1a7a407450e13e
Anthony Langford <anthony.langford@gmail.com>
2018-08-17 09:45:23 -0400

    add filter actions to index

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,4 @@ export { default as uiActions } from './ui';
 export { default as sortActions } from './sort';
 export { default as locationActions } from './location';
 export { default as storeActions } from './store';
+export { default as filterActions } from './filter';
