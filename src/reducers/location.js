6943b4491dd2f21a978e88ec029965e2c048c227
Anthony Langford <anthony.langford@gmail.com>
2018-08-16 21:31:48 -0400

    create location reducers

---
+++ src/reducers/location.js
@@ -0,0 +1,19 @@
+export default function location(
+  state = {},
+  action) {
+  switch (action.type) {
+  case 'RECEIVE_LOCATION' : {
+    return {
+      ...state,
+      lat: action.lat,
+      lon: action.lon
+    }
+  }
+  case 'CLEAR_STORE' : {
+    return {};
+  }
+  default: {
+    return state;
+  }
+  }
+}
