ad379e179a8fac9c87bf26b5ae5f21edbcff94cd
Anthony Langford <anthony.langford@gmail.com>
2018-08-17 09:45:00 -0400

    cleanup ✨

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux'
 // https://medium.com/@gethylgeorge/understanding-how-redux-thunk-works-72de3bdebc50
 // https://cdn-images-1.medium.com/max/1600/1*eByw4NguNH8nK0MA_Q5Fow.png
-import thunkMiddleware from 'redux-thunk';
+import thunkMiddleware from 'redux-thunk'
 
 // Import the root reducer
-import rootReducer from './reducers';
+import rootReducer from './reducers'
 
 // Create an object for the default state
 const initialState = {
@@ -27,4 +27,4 @@ const store = createStore(
   composeEnhancers(environmentMiddleware)
 )
 
-export default store;
+export default store
