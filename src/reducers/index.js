7edfb665de3a72d0e57922180af19814362c5312
Anthony Langford <anthony.langford@gmail.com>
2018-08-17 09:48:19 -0400

    add filters to reducer index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,15 @@ import ui from './ui'
 import sort from './sort'
 import location from './location'
 import stores from './stores'
+import filters from './filters'
 
 const rootReducer = combineReducers({
   products,
   ui,
   sort,
   location,
-  stores
+  stores,
+  filters
 })
 
 export default rootReducer
