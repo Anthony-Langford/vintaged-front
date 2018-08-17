d7e96fa45bb51cfa4fbe154924708edbcd26aac6
Anthony Langford <anthony.langford@gmail.com>
2018-08-16 21:25:15 -0400

    add product, location, and store actions to index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
-export { default as commonActions } from './common';
+export { default as productActions } from './product';
 export { default as uiActions } from './ui';
 export { default as sortActions } from './sort';
+export { default as locationActions } from './location';
+export { default as storeActions } from './store';
