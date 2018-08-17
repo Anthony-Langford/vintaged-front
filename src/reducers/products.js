33a99168512c95d077900a3c889a2a2cf4658e25
Anthony Langford <anthony.langford@gmail.com>
2018-08-16 21:31:35 -0400

    create product reducers

---
+++ src/reducers/products.js
@@ -0,0 +1,38 @@
+export default function products(
+  state = {
+    isFetching: false,
+  },
+  action) {
+  switch (action.type) {
+  case 'REQUEST_WINES' : {
+    return {
+      ...state,
+      isFetching: true,
+    };
+  }
+  case 'RECEIVE_WINES' : {
+    return {
+      ...state,
+      isFetching: false,
+      wines: action.wines,
+      lastUpdated: action.receivedAt,
+    };
+  }
+  case 'RECEIVE_WINE' : {    
+    return {
+      ...state,
+      isFetching: false,
+      [action.wine.id]: action.wine,
+      lastUpdated: action.receivedAt,
+    };
+  }
+  case 'CLEAR_STORE' : {
+    return {
+      isFetching: false,
+    };
+  }
+  default : {
+    return state;
+  }
+  }
+}
