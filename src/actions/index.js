5d8f37c5fde0f0a471a3e3ae51078433c9e1c5cb
Anthony Langford <anthony.langford@gmail.com>
2018-08-23 12:23:20 -0400

    remove store from action index

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,5 +2,4 @@ export { default as productActions } from './product';
 export { default as uiActions } from './ui';
 export { default as sortActions } from './sort';
 export { default as locationActions } from './location';
-export { default as storeActions } from './store';
 export { default as filterActions } from './filter';
