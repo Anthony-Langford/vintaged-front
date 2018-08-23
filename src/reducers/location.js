d7eb0bc7f13567f2a2a86c7ff3638d7d219d971e
Anthony Langford <anthony.langford@gmail.com>
2018-08-23 12:30:29 -0400

    move store reducer into location

--- src/reducers/location.js
+++ src/reducers/location.js
@@ -1,10 +1,23 @@
-export default function location(state = {}, action) {
+export default function location(state = {
+  isFetching: false,
+}, action) {
   switch (action.type) {
   case 'RECEIVE_LOCATION':
     return {
       ...state,
       lat: action.lat,
       lon: action.lon
+    }
+
+  case 'RECEIVE_STORE':
+    return {
+      ...state,
+      nearestStore: {
+        id: action.store.id,
+        name: action.store.name,
+        vintages: action.store.available_vintages,
+        lastUpdated: action.receivedAt,
+      },
     }
 
   case 'CLEAR_STORE':
